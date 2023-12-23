2a9d696bffa0f223626d76309b886950d4bcfa31
2cmo <111031844+2cmo@users.noreply.github.com>
2023-12-22 21:30:30 -0500

    Update index.js

--- Assests/JS/index.js
+++ Assests/JS/index.js
@@ -1,45 +1,24 @@
 var says = [
-  "Is That a Railin Reference?",
-  "Please Stop Being Racist In The DISC0RD",
-  "tinyurl.com/RailinDev",
   "Your one and only Unbl0cked Playground",
-  "I hate working on railin",
+  "I hate working on flyoota",
   "Better than Bypass",
-  "GD 2.2 OCTOBER 2023!!!",
-  "yowoodTodd",
-  "Bot",
-  "RAILIN 2.0!",
-  "stop asking where disc0rd is. its in widgetbot",
-  "blogs was usless",
-  "so was secrets",
-  "Tempus fugit ⏰",
   "yes its open-source",
   "no you shouldnt skid it",
   "what do i add?",
-  "*you're* 👆🤓",
   "idk how many games are in this lol",
   "optimized by ChatGPT",
   "archiving piksulh soon",
-  "Cooper thanks for loving railin!",
-  "i cant use fontawesome because of you guys :(",
   "LunarOS 🔛🔝",
   "i need ads on this site cuz i need money 😮",
-  "uh",
-  "my coder friends copying me and hes better at coding then me",
   "Schools Pretty Boring",
   "This Websites Trash", 
   "i took too long making this :(", 
-  "AOSUDHASIUD4u3ghiuj34hgi34gH", 
-  "hotline bling", 
-  "all 1v1lol's are blocked at my school", 
+  "working on adding the undated 1v1.lol soon", 
   "the person who made this websites pretty cool", 
   "what day is it?", 
-  "fortnite battl pass", 
   "i made this on the weekend :((((",
-  "hiiii aiden :)",
+  "hi reed",
   "i hate myself",
-  "im in love with a emo gorl 💅",
-  "amog",
   "💀",
   "hey e-kitten",
 ];
@@ -57,6 +36,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     .then((data) => {
       ipAddress = data.YourFuckingIPAddress;
       says.push(`umm your ip: ${ipAddress}`);
+      splashText();
+
+      function splashText() {
+  document.querySelector(".Index-SplashText").innerHTML =
+    says[Math.floor(Math.random() * says.length)];
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  splashText();
+
+  fetch("https://ipv4.wtfismyip.com/json")
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddress = data.YourFuckingISP;
+      says.push(`umm your fucking isp: ${ipAddress}`);
       splashText();
     });
 });
