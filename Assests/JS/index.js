b9a712c30ecabb6fbe28333ccc7eabfda9a4e7dc
2cmo <111031844+2cmo@users.noreply.github.com>
2024-02-08 07:33:41 -0500

    Update index.js

--- Assests/JS/index.js
+++ Assests/JS/index.js
@@ -1,2 +1,20 @@
 var says = [
   "Levi and Grant don't know math 🤣",
+];
+
+function splashText() {
+  document.querySelector(".Index-SplashText").innerHTML =
+    says[Math.floor(Math.random() * says.length)];
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  splashText();
+
+  fetch("")
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddress = data.;
+      says.push(`?: ${ipAddress}`);
+      splashText();
+    });
+});
